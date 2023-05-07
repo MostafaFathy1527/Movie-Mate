5261351ef2f8489b4be1ea77685a1477d8290fc9
SupeeerMario <khaledshaaban926@gmail.com>
2023-05-07 08:36:20 +0300

    ararararararaaaaaaaaaa

--- app/src/main/java/com/example/moviemate/SearchResultsActivity.java
+++ app/src/main/java/com/example/moviemate/SearchResultsActivity.java
@@ -25,7 +25,7 @@ public class SearchResultsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_results);
+        setContentView(R.layout.activity_search_movie);
 
 
 
